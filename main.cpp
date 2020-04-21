fb1de9a29884e92f2c91068806a5fdc8ba88070a
alexvsezanyato <stepin.aleksandr.dj@gmail.com>
2020-04-21 19:41:17 +0700

    Add a wrong terminal command handler

--- main.cpp
+++ main.cpp
@@ -9,4 +9,11 @@ void welcome(std::string name)
 int main(int argc, char* argv[])
 {
 	if (argc == 2) welcome(argv[1]);
+	else
+	{
+		std::cout << "You must type your name after the program name \n";
+		std::cout << "<program name> <your name> \n";
+		std::cout << "The program has stopped \n";
+		return 1;
+	}
 }
